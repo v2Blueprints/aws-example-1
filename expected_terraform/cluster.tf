af5abfa3ba9440751d71098a0b28c0ffbfc867d1
James Vodanovich <james@jvodan.com>
2022-05-18 13:11:33 +1000

    add more

--- expected_terraform/cluster.tf
+++ expected_terraform/cluster.tf
@@ -1,6 +1,9 @@
 resource "aws_kms_key" "cluster-key" {
   description             = "cluster-key"
-  deletion_window_in_days = 10
+  deletion_window_in_days = 10,
+  is_enabled = true
+  enable_key_rotation = false
+  multi_region = false
 }
 
 resource "aws_cloudwatch_log_group" "Dougs-cluster-logs" {
