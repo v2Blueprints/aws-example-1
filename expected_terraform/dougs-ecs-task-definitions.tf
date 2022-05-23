21b30c0e8d27d301372a7819547b4e31a6bd2227
James Vodanovich <james@jvodan.com>
2022-05-23 11:28:22 +1000

    add move tf

---
+++ expected_terraform/dougs-ecs-task-definitions.tf
@@ -0,0 +1,69 @@
+resource "aws_ecs_task_definition" "doug_ecs_task_definitions" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "abs"
+      image     = "abs"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "billing-engine"
+      image     = "billing-engine"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    },
+    {
+      name      = "longitude"
+      image     = "longitude"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    },
+    {
+      name      = "sura"
+      image     = "sura"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    },
+    {
+      name      = "opta"
+      image     = "opta"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    }
+  ])
