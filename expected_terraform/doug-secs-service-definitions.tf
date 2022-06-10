2ff144edb4700a5c97252206cc637ccefff1596a
James Vodanovich <james@jvodan.com>
2022-06-10 15:10:08 +1000

    fix tf

--- expected_terraform/doug-secs-service-definitions.tf
+++ expected_terraform/doug-secs-service-definitions.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_service" "billing-engine" {
   name            = "billing-engine"
-  cluster         = aws_ecs_cluster.aws_ecs_cluster.id
+  cluster         = aws_ecs_cluster.Dougs-Cluster.id
   task_definition = aws_ecs_task_definition.billing-engine.arn
   desired_count   = 3
   iam_role        = aws_iam_role.foo.arn
@@ -16,7 +16,7 @@ resource "aws_ecs_service" "billing-engine" {
   
   resource "aws_ecs_service" "longitude" {
   name            = "longitude"
-  cluster         = aws_ecs_cluster.aws_ecs_cluster.id
+  cluster         = aws_ecs_cluster.Dougs-Cluster.id
   task_definition = aws_ecs_task_definition.longitude.arn
   desired_count   = 3
   iam_role        = aws_iam_role.foo.arn
@@ -30,7 +30,7 @@ resource "aws_ecs_service" "billing-engine" {
   
   resource "aws_ecs_service" "sura" {
   name            = "sura"
-  cluster         = aws_ecs_cluster.aws_ecs_cluster.id
+  cluster         = aws_ecs_cluster.Dougs-Cluster.id
   task_definition = aws_ecs_task_definition.sura.arn
   desired_count   = 3
   iam_role        = aws_iam_role.foo.arn
@@ -43,7 +43,7 @@ resource "aws_ecs_service" "billing-engine" {
   }
       resource "aws_ecs_service" "opta" {
   name            = "opta"
-  cluster         = aws_ecs_cluster.aws_ecs_cluster.id
+  cluster         = aws_ecs_cluster.Dougs-Cluster.id
   task_definition = aws_ecs_task_definition.opta.arn
   desired_count   = 3
   iam_role        = aws_iam_role.foo.arn
