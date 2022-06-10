39bc2c342021355584002454fb56702c87bf55e7
James Vodanovich <james@jvodan.com>
2022-06-10 15:07:29 +1000

    drop conf block for the moment

--- expected_terraform/cluster.tf
+++ expected_terraform/cluster.tf
@@ -12,15 +12,5 @@ resource "aws_cloudwatch_log_group" "Dougs-cluster-logs" {
 resource "aws_ecs_cluster" "Dougs-Cluster" {
   name = "Dougs-Cluster"
 
-  configuration {
-    execute_command_configuration {
-      kms_key_id = aws_kms_key.cluster-key.arn
-      logging    = "OVERRIDE"
-
-      log_configuration {
-        cloud_watch_encryption_enabled = true
-        cloud_watch_log_group_name = aws_cloudwatch_log_group.Dougs-cluster-logs.name
-      }
-    }
-  }
+ 
 }
